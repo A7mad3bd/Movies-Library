18c18a0aa449efb323cac4441727ff516b7127e7
A7mad3bd <ahmadabd398@gmail.com>
2022-02-02 21:05:18 +0200

    Task13-edit

---
+++ schema.sql
@@ -0,0 +1,13 @@
+
+
+DROP TABLE IF EXISTS myfavmovietable;
+
+CREATE TABLE IF NOT EXISTS myfavmovietable (
+
+ id SERIAL PRIMARY KEY,
+title VARCHAR(255),
+release_date VARCHAR(255),
+vote_average VARCHAR(255),
+overview VARCHAR(255)
+
+);
